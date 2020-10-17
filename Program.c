2477af463e8e675eb7bab81dcdd608fd948476b5
Shreya Jadhav <67734649+shreyajadhav@users.noreply.github.com>
2020-10-17 10:36:33 +0530

    Update Program.c

--- Program.c
+++ Program.c
@@ -5,7 +5,7 @@ int main()
     char name,country,skill;
     printf("Welcome to Hacktoberfest 2020\n");
     printf("Enter your name : ");//Get User name
-    scanf("%d",&name);
+    scanf("%s",&name);
     printf("Enter your Country of Residance : ");//Get user country
     scanf("%s",&country);
     printf("Enter your Age : ");//Get user Age
